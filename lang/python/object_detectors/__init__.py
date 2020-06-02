f8b7aaf9646a7deba4e7e63a6e105ec78db68790
Nick <nicholas.c.burnett@gmail.com>
2020-06-02 09:25:56 -0400

    Refactor for PIP

--- lang/python/object_detectors/__init__.py
+++ lang/python/object_detectors/__init__.py
@@ -2,20 +2,55 @@ import cv2
 import logging
 import numpy as np
 import os
+import requests
 import sys
 import time
 
 from pydarknet import Detector, Image
 # from vidstreamer import Streamer
 
-DARKPY_PATH = "./darknet"
+DARKPY_PATH = os.path.join(sys.prefix, "darknet-files")
+BLOCK_SIZE = 1024
 
-CFG = os.path.join(DARKPY_PATH, "cfg", "yolov3-tiny.cfg")
-WEIGHTS = os.path.join(DARKPY_PATH, "weights", "yolov3-tiny.weights")
-DATA = os.path.join(DARKPY_PATH, "cfg", "coco.data")
+darknet_config = {
+    "yolov3": {
+        "weights": os.path.join(DARKPY_PATH, "weights/yolov3.weights"),
+        "config": os.path.join(DARKPY_PATH, "cfg/yolov3.cfg"),
+        "data": os.path.join(DARKPY_PATH, "cfg/coco.data"),
+        "weights_url": "https://pjreddie.com/media/files/yolov3.weights" 
+    },
+    "yolov3-tiny": {
+        "weights": os.path.join(DARKPY_PATH, "weights/yolov3-tiny.weights"),
+        "config": os.path.join(DARKPY_PATH, "cfg/yolov3-tiny.cfg"),
+        "data": os.path.join(DARKPY_PATH, "cfg/coco.data"),
+        "weights_url": "https://pjreddie.com/media/files/yolov3-tiny.weights"
+    },
+}
+
+def download_model(model):
+    r = requests.get(darknet_config[model]["weights_url"], stream=True)
+    if not os.path.exists(os.path.join(DARKPY_PATH, "weights")):
+        os.makedirs(os.path.join(DARKPY_PATH, "weights"))
+
+    with open("darknet_config[model]['weights_url']", 'wb') as f:
+        for data in r.iter_content(BLOCK_SIZE):
+            f.write(data)
+    logging.info("File {!s} successfully downloaded. Size: {!s}".format(darknet_config[model]["weights_url"], int(r.headers.get('content-length', 0))))
 
 class Darknet:
-    def __init__(self, config_path=CFG, weights_path=WEIGHTS, data_path=DATA):
+    def __init__(self, model=None, config_path=None, weights_path=None, data_path=None):
+        if not config_path and model:
+            config_path = darknet_config[model]["config"]
+        if not weights_path and model:
+            weights_path = darknet_config[model]["weights"]
+            if not os.path.exists(weights_path):
+                download_model(model)
+        if not data_path and model:
+            data_path = darknet_config[model]["data"]
+
+        
+            
+        
         self.net = Detector(bytes(config_path, encoding="utf-8"), bytes(weights_path, encoding="utf-8"), 0, 
                 bytes(data_path, encoding="utf-8"))
 
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+def iter_protos(parent=None):
+    for root, _, files in os.walk('proto'):
+        if not files:
+            continue
+        dest = root if not parent else os.path.join(parent, root)
+        yield dest, [os.path.join(root, f) for f in files]
+
+# def get_all_files
+
+pkg_name = 'video-analytics'
+
+setup(name=pkg_name, 
+        package_dir={
+            '':'lang/python',
+            },
+        version='0.1.0',
+        description='Collection of video analytics for easy integration into python applications',
+        author='Nick Burnett',
+        author_email='nicholas.c.burnett@gmail.com',
+        url='github.com/pvjammer/video-analytics',
+        license='Apache License, Version 2.0',
+        packages=["object_detectors"],
+        data_files=[
+            ("darknet-files", ["cfg/yolov3.cfg",
+                               "cfg/yolov3-tiny.cfg",
+                               "cfg/coco.data",
+                               "cfg/coco.names"])
+        ],
+        install_requires=[
+          'cython',
+          'opencv-python>=4.2.0.0',
+          'numpy>=1.18.0',
+          'yolo34py>=0.1',
+        #   'yolo34py-gpu>=0.1'
+            ],
+        data_files=list(iter_protos(pkg_name)),
+        py_modules = [
+            'object_detectors.__init__'
+            ])
+
